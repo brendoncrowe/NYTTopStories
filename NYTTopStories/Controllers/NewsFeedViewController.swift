848bf6c0411f13f3c07235eacca3e26f9ca0cf51
Brendon Crowe <brendon.m.crowe@gmail.com>
2023-03-18 13:39:09 -0400

    added collection view and search bar to news feed controller

--- NYTTopStories/Controllers/NewsFeedViewController.swift
+++ NYTTopStories/Controllers/NewsFeedViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class NewsFeedViewController: UIViewController {
+    
+    private let newsFeedView = NewsFeedView()
+    
+    override func loadView() {
+        view = newsFeedView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .systemGreen
+        view.backgroundColor = .systemBackground
+        newsFeedView.collectionView.dataSource = self
+        newsFeedView.collectionView.delegate = self
+        newsFeedView.collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "articleCell")
     }
+}
 
+extension NewsFeedViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = newsFeedView.collectionView.dequeueReusableCell(withReuseIdentifier: "articleCell", for: indexPath)
+        cell.backgroundColor = .systemGray3
+        return cell
+    }
+}
 
+extension NewsFeedViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let maxSize = (view.window?.windowScene?.coordinateSpace.bounds.size)!
+        let itemWidth: CGFloat = maxSize.width
+        let itemHeight: CGFloat = maxSize.height * 0.30
+        return CGSize(width: itemWidth, height: itemHeight)
+    }
 }
---
+++ NYTTopStories/Views/NewsFeedView.swift
@@ -0,0 +1,62 @@
+//
+//  NewsFeedView.swift
+//  NYTTopStories
+//
+//  Created by Brendon Crowe on 3/18/23.
+//
+
+import UIKit
+
+class NewsFeedView: UIView {
+    
+    public lazy var searchBar: UISearchBar = {
+        let sb = UISearchBar()
+        sb.translatesAutoresizingMaskIntoConstraints = false
+        sb.autocapitalizationType = .none
+        sb.placeholder = "search for article"
+        return sb
+    }()
+    
+    public lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        return cv
+    }()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        setSearchBarConstraints()
+        setCollectionViewConstraints()
+    }
+    
+    private func setSearchBarConstraints() {
+        addSubview(searchBar)
+        NSLayoutConstraint.activate( [
+            searchBar.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            searchBar.leadingAnchor.constraint(equalTo: leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+    }
+    
+    private func setCollectionViewConstraints() {
+        addSubview(collectionView)
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+    }
+}
