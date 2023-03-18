f5363f6ef549cbc1d808f6c188fbd5251efd8b1c
Brendon Crowe <brendon.m.crowe@gmail.com>
2023-03-18 12:51:43 -0400

    Initial Commit

---
+++ NYTTopStories/Controllers/TopStoriesTabController.swift
@@ -0,0 +1,17 @@
+//
+//  TopStoriesTabController.swift
+//  NYTTopStories
+//
+//  Created by Brendon Crowe on 3/18/23.
+//
+
+import UIKit
+
+class TopStoriesTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .cyan
+    }
+}
