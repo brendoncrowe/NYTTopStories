715cfce0a66afcbc23aa618e4faf72dda08c1140
Brendon Crowe <brendon.m.crowe@gmail.com>
2023-03-18 15:39:35 -0400

    added api client

--- NYTTopStories/NetworkServices/NYTTopStoriesAPIClient.swift
+++ NYTTopStories/NetworkServices/NYTTopStoriesAPIClient.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import NetworkHelper
+
+
+struct NetworkAPIClient {
+    
+    static func fetchItems(for section: String, completion: @escaping (Result<[Article], AppError>) -> ()) {
+        let endpoint = "https://api.nytimes.com/svc/topstories/v2/us.json?api-key="
+        guard let url = URL(string: endpoint) else {
+            completion(.failure(.badURL(endpoint)))
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        NetworkHelper.shared.performDataTask(with: request) { result in
+            switch result {
+            case .failure(let appError):
+                completion(.failure(.networkClientError(appError)))
+            case .success(let data):
+                do {
+                    let searchResults = try JSONDecoder().decode(TopStories.self, from: data)
+                    let articles = searchResults.results
+                    completion(.success(articles))
+                } catch {
+                    completion(.failure(.decodingError(error)))
+                }
+            }
+        }
+    }
+}
